af803508a12c8aa192caec919ac2ba2dd688c743
Sai Akhil Golla <saiakhil1011@gmail.com>
2025-01-08 18:33:47 -0500

    added scripts and functions to connect to sqlite db, create table and insert rows in the table

---
+++ src/database/db_operations.py
@@ -0,0 +1,36 @@
+import sqlite3
+from db_connector import get_sqlite3_connector
+
+def insert_transactions(conn:sqlite3.Connection, row_data:dict):
+	#insert new rows into Transactions table
+	try:
+		with conn:
+			conn.executemany('''
+							INSERT INTO Transactions VALUES(
+							:MonthYear,
+							:Date,
+							:Description,
+							:SubDescription,
+							:TransactionType,
+							:Amount,
+							:AccountType,
+							:AccountName,
+							:Category)
+							''', row_data)
+	except sqlite3.IntegrityError:
+		print("couldn't add a row twice")
+	return()
+
+
+
+conn = get_sqlite3_connector("./src/database/MonthlyExpenses.db")
+data = {"MonthYear": "November 2024",
+		"Date": "2024-11-03",
+		"Description": "test",
+		"SubDescription": "test_desc",
+		"TransactionType": "debit",
+		"Amount": 1500,
+		"AccountType": "Chequing",
+		"AccountName": "preferred Package",
+		"Category": "example"}
+insert_transactions(conn, data)
