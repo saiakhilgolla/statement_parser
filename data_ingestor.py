ebb2fc3a8cb33f57535072697c185781134593a0
Sai Akhil Golla <saiakhil1011@gmail.com>
2025-02-20 20:32:03 -0500

    refactored data_ingestor code to work with new scripts

--- data_ingestor.py
+++ data_ingestor.py
@@ -1,45 +1,35 @@
 import os
-from data_processing.csv_process_OLD import process_csv
+from src.data_processing.data_processor import FileProcessorFactory, DataProcessor
 from src.data_categorization.gpt_categorizer import CategorizeTransaction
-from database.db_operations_old import insert_transactions
+from src.database.db_config import session_local
 from src.utils.path_utils import get_file_list, validate_file_list
-from src.database.db_connector import get_sqlite3_connector
+from src.utils.file_utils import load_config, is_csv_file
 
-# TODO: ADD DEBIT AND CREDIT TRANSACTIONS IN DIFFERENT TABLES TO KEEP TRACK OF BALANCE
 
-# Configuration file paths
-CONFIG = {
-    "debit_path": "data/debit_accounts",
-    "credit_path": "data/credit_accounts",
-    "db_path": "src/database/MonthlyExpenses.db",
-}
+# Load file and database paths from config
+config = load_config("config/file_config.json")
+file_paths = config['FILE_PATHS']
 
-def is_csv_file(file_path):
-    """Validate if the file is a CSV file."""
-    return file_path.endswith(".csv")
+for csv_file_path in file_paths:
+	if not is_csv_file(csv_file_path):
+		print(f"Skipping non-CSV file: {csv_file_path}")
+		continue
 
-def process_and_categorize_files(file_paths, required_columns, db_connection):
+def process_and_categorize_files(file_path, required_columns, db_connection):
     """Process CSV files, categorize transactions, and insert them into the database."""
-    for csv_file_path in file_paths:
-        if not is_csv_file(csv_file_path):
-            print(f"Skipping non-CSV file: {csv_file_path}")
-            continue
 
-        processed_df = process_csv(csv_file_path)
+	file_processor = FileProcessorFactory.get_file_processor('csv', file_path)
+	processed_df = DataProcessor(file_processor).process_file()
 
-        # Categorize transactions
-        categories = [
-            CategorizeTransaction(row.to_string()).get_category()
-            for _, row in processed_df[required_columns].iterrows()
-        ]
-        processed_df['Category'] = categories
 
-        # Prepare data for database insertion
-        data_load = tuple(processed_df.to_dict(orient="records"))
-        #print(data_load)
+	# Categorize transactions by passing each row with required cols as a string to categorizer
+	categories = [
+		CategorizeTransaction(row.to_string()).get_category()
+		for _, row in processed_df[required_columns].iterrows()
+	]
+	processed_df['Category'] = categories
+    return (processed_df)
 
-        # Insert transactions into the database
-        insert_transactions(db_connection, data_load)
 
 def main():
     print("called main function")
